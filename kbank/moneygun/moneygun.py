27061f122787a5faece3af656f808c8677f18830
cg10036 <cg10036@gmail.com>
2025-06-01 14:46:11 +0900

    update

--- kbank/moneygun/moneygun.py
+++ kbank/moneygun/moneygun.py
@@ -1,7 +1,15 @@
 import re
+import glob
+import os
 
-with open('moneygun.txt', 'r', encoding="utf-8") as f:
-    txt = f.read()
+# csv 폴더 안의 모든 *.csv 파일을 읽어서 txt 변수에 저장
+txt = ""
+csv_files = glob.glob(os.path.join('csv', '*.csv'))
+
+for csv_file in csv_files:
+    with open(csv_file, 'r', encoding="utf-8") as f:
+        file_content = f.read()
+        txt += file_content + "\n"
 
 urls = re.findall(
     r"용돈을 선물 받았어요👏 \n얼마가 들어 있을까요\?\n(https://m\.kbanknow\.com/banking/k/[a-zA-Z0-9]+)", txt)
@@ -19,3 +27,8 @@ for url in urls:
 
 with open("moneygun.new.txt", "w", encoding="utf-8") as f:
     f.write("\n".join([*old_urls, *final_urls]))
+
+with open("moneygun.format.txt", "w", encoding="utf-8") as f:
+    f.write("\"")
+    f.write("\",\n\"".join([*old_urls, *final_urls]))
+    f.write("\"")
